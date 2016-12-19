e3679f935cffdcae1d736d324acf966c19f24dfd
g0ma <gomasiopan@gmail.com>
2016-12-19 22:44:24 +0900

    プロジェクト ファイルを追加します。

---
+++ TipsList/Viewer.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TipsList
+{
+    partial class Viewer
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ViewList = new System.Windows.Forms.ListBox();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CheckButton = new System.Windows.Forms.Button();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.Message = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ViewList
+            // 
+            this.ViewList.BackColor = System.Drawing.Color.White;
+            this.ViewList.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ViewList.FormattingEnabled = true;
+            this.ViewList.ItemHeight = 17;
+            this.ViewList.Location = new System.Drawing.Point(0, 50);
+            this.ViewList.Name = "ViewList";
+            this.ViewList.Size = new System.Drawing.Size(294, 357);
+            this.ViewList.TabIndex = 0;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.SaveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SaveButton.Location = new System.Drawing.Point(12, 7);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(33, 30);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "💾";
+            this.SaveButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.SaveButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // CheckButton
+            // 
+            this.CheckButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckButton.Location = new System.Drawing.Point(215, 7);
+            this.CheckButton.Name = "CheckButton";
+            this.CheckButton.Size = new System.Drawing.Size(69, 30);
+            this.CheckButton.TabIndex = 2;
+            this.CheckButton.Text = "✔";
+            this.CheckButton.UseVisualStyleBackColor = true;
+            this.CheckButton.Click += new System.EventHandler(this.CheckButton_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.BackColor = System.Drawing.Color.AliceBlue;
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Message});
+            this.statusStrip.Location = new System.Drawing.Point(0, 409);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip.Size = new System.Drawing.Size(294, 22);
+            this.statusStrip.SizingGrip = false;
+            this.statusStrip.TabIndex = 3;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // Message
+            // 
+            this.Message.Name = "Message";
+            this.Message.Size = new System.Drawing.Size(43, 17);
+            this.Message.Text = "status";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Meiryo UI", 1F);
+            this.label1.Location = new System.Drawing.Point(10, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 1);
+            this.label1.TabIndex = 4;
+            // 
+            // Viewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(294, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.CheckButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.ViewList);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Viewer";
+            this.Text = "TipsList";
+            this.Load += new System.EventHandler(this.Viewer_Load);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ViewList;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button CheckButton;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel Message;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TipsList/Viewer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TipsList
+{
+    public partial class Viewer : Form
+    {
+        string FilePath = @"E:\game.txt";
+        public Viewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Viewer_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader reader = new StreamReader(FilePath, Encoding.UTF8);
+                string str = reader.ReadToEnd();
+                reader.Close();
+                string[] ReadItems = str.Split(new[] { "\r\n" }, StringSplitOptions.None);
+
+                foreach (var item in ReadItems)
+                {
+                    ViewList.Items.Add(item);
+                }
+
+                Message.Text = "読込完了";
+            }
+            catch (FileNotFoundException ex)
+            {
+                Message.Text = ex.Message;
+            }
+        }
+
+        private void CheckButton_Click(object sender, EventArgs e)
+        {
+            if (ViewList.SelectedIndex != -1)
+            {
+                ViewList.Items.RemoveAt(ViewList.SelectedIndex);
+                Message.Text = "1行削除しました";
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(FilePath, false, Encoding.UTF8);
+                foreach(var item in ViewList.Items)
+                {
+                    writer.WriteLine(item);
+                }
+                writer.Close();
+                Message.Text = "保存しました";
+            }
+            catch
+            {
+                Message.Text = "保存失敗";
+            }
+        }
+    }
+}
